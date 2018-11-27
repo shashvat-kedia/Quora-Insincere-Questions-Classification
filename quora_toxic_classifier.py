0b9350560e81f67e0b6e26ce82f74b1d023bc9df
sd1998 <shashvat51@gmail.com>
2018-11-27 10:46:55 +0530

    Chunking of data added

--- quora_toxic_classifier.py
+++ quora_toxic_classifier.py
@@ -12,32 +12,6 @@ import pickle
 import datetime
 import tensorflow as tf
 
-#Conversion to different implementations for fitting model on the complete dataset but transforming only on chunks of it.
-
-def load_and_preprocess(min_frequency=0,vocab_processor=None):    
-    if vocab_processor is None:
-        vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(max_length,min_frequency=min_frequency)
-        if(not os.path.isfile('processed/vocab')):
-            for idx,row in dataset_train.iterrows():
-                vocab_processor= vocab_processor.fit(str(row['question_text']))
-            for idx,row in dataset_train.iterrows():
-                data.append(list(vocab_processor.transform(str(row['question_text']))))
-            data = np.array(data)
-        else:
-            vocab_processor.restore('processed/vocab')
-            for idx,row in dataset_train.iterrows():
-                data.append(list(vocab_processor.transform(str(row['question_text']))))
-            data = np.array(data)
-    data_size = len(data)
-    shuffle_index = np.random.permutation(np.arange(data_size)) 
-    data = data[shuffle_index]
-    labels = labels[shuffle_index]
-    lengths = lengths[shuffle_index]
-    endtime = time.time()
-    print("Time to load and preprocess")
-    print(endtime - starttime)
-    return data,labels,lengths,vocab_processor
-   
 def get_batch(data,labels,lengths,batch_size,epochs):
     assert len(data) == len(labels) == len(lengths)
     no_batches = len(data) // batch_size
@@ -106,7 +80,7 @@ def preprocess(path):
         print('Preprocessing test dataset')
         savepath = 'dataset/processed_test.csv'
     stop = set(stopwords.words('english'))
-    print(datase.shape)
+    print(dataset.shape)
     print(dataset.head())
     dataset = dataset.drop(['qid'],axis=1)
     #stemmer = SnowballStemmer('english')
@@ -129,75 +103,100 @@ def preprocess(path):
     
 def create_vocabulary(min_frequency=0):
     starttime = time.time()
-    processed_dataset = pd.read_csv('/dataset/processed_train.csv')
+    dataset = pd.read_csv('dataset/processed_train.csv')
     dataset = dataset.drop(dataset.columns[0],axis=1)
     print(dataset.shape)
     print(dataset.head())
     max_length = 0
     for idx,row in dataset.iterrows():
+        length = len(str(row['question_text']).strip().split())
         if length > max_length:
             max_length = length
     print("Max length:- ")
     print(max_length)
+    with open('processed/max_length.txt','w') as file:
+        file.write(str(max_length))  
     vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(max_length,min_frequency=min_frequency)
     for idx,row in dataset.iterrows():
         vocab_processor.fit(str(row['question_text']))
-    vocab_processor.save('/processed/vocab')
+    vocab_processor.save('processed/vocab')
     endtime = time.time()
     print('Time to create vocabulary')
     print(endtime - starttime)
 
+def get_processed_batch_data(data,vocab_processor,count):
+    starttime = time.time()
+    lengths = []
+    labels = []
+    data = []
+    for row,idx in data.iterrows():
+        lengths.append(len(str(row['question_text']).strip().split(' ')))
+        labels.append(row['target'])
+        data.append(list(vocab_processor.transform(str(row['question_text']))))
+    lengths = np.array(lengths)
+    labels = np.array(labels)
+    data = np.array(data)
+    endtime = time.time()
+    print('Time to create batch {:g}'.format(count))
+    print(endtime - starttime)
+    return data,labels,lengths
+
 def train():
     if(not os.path.isfile('dataset/processed_train.csv')):
         preprocess('dataset/train.csv')
-    if(not os.path.isfile('dataset.processed_csv')):
+    if(not os.path.isfile('dataset/processed_test.csv')):
         preprocess('dataset/test.csv')
     if(not os.path.isfile('processed/vocb')):
         create_vocabulary()
-    #Batching to be implemented here to counter Main Memory overload issues
-    X_train,X_test,y_train,y_test,train_lengths,valid_lengths = train_test_split(X,y,lengths,test_size=0.2,random_state=0)
-    train_data = get_batch(X_train,y_train,train_lengths,32,50)
-    with tf.Graph().as_default():
-        with tf.Session() as sess:
-            classifier = LSTM(2,len(vocab_processor.vocabulary_._mapping),300,2,0.001)
-            global_step = tf.Variable(0,name='global_step',trainable=False)
-            learning_rate = tf.train.exponential_decay(1e-3,global_step,100000,1,staircase=True)
-            optimizer = tf.train.AdamOptimizer(learning_rate)
-            grad_and_vars = optimizer.compute_gradients(classifier.cost)
-            train_op = optimizer.apply_gradients(grad_and_vars,global_step=global_step)
-            sess.run(tf.global_variables_initializer())
-            def run(train_input,is_training=True):
-                x_data,y_data,length_data = train_input
-                fetches = {'step': global_step,
-                       'cost': classifier.cost,
-                       'accuracy': classifier.accuracy,
-                       'learning_rate': learning_rate,
-                       'final_state': classifier.final_state}
-                feed_dict = {classifier.x: x_data,
-                         classifier.y: y_data,
-                         classifier.sequence_length: length_data,
-                         classifier.batch_size: len(x_data)}
-                vars = sess.run(fetches, feed_dict)
-                step = vars['step']
-                cost = vars['cost']
-                accuracy = vars['accuracy']
-                if is_training:
-                    fetches['train_op'] = train_op
-                    feed_dict[classifier.keep_prob] = 0.5
-                else:
-                    feed_dict[classifier.keep_prob] = 1.0
-                time_str = datetime.datetime.now().isoformat()
-                print("{}: step: {}, loss: {:g}, accuracy: {:g}".format(time_str, step, cost, accuracy))
-                return accuracy
-            for train_input in train_data:
-                run(train_input,is_training=True)
-                current_step = tf.train.global_step(sess,global_step)
-            
-def test():
-    vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor().restore('processed/vocab')
-    X,y,lengths,_ = load_and_preprocess(vocab_processor=vocab_processor)
-    with tf.Graph().as_default():
-        with tf.Session() as sess:
-            
+    max_length = 0
+    with open('processed/max_length.txt','r') as file:
+        max_length = int(file.read())
+    vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(max_length,min_frequency=0).restore('/processed/vocab')
+    chunksize = 10 ** 4
+    count = 1
+    for data in pd.read_csv('dataset/processed_train.csv',chunksize=chunksize):
+        print('Batch no.:-')
+        print(count)
+        count += 1
+        print(data.info(memory_usage='deep'))
+        data = data.drop(data.columns[0],axis=1)
+        X,y,lengths = get_processed_batch_data(data,vocab_processor,count)
+        X_train,X_test,y_train,y_test,train_lengths,valid_lengths = train_test_split(X,y,lengths,test_size=0.2,random_state=0)
+        train_data = get_batch(X_train,y_train,train_lengths,32,50)
+        with tf.Graph().as_default():
+            with tf.Session() as sess:
+                classifier = LSTM(2,len(vocab_processor.vocabulary_._mapping),300,2,0.001)
+                global_step = tf.Variable(0,name='global_step',trainable=False)
+                learning_rate = tf.train.exponential_decay(1e-3,global_step,100000,1,staircase=True)
+                optimizer = tf.train.AdamOptimizer(learning_rate)
+                grad_and_vars = optimizer.compute_gradients(classifier.cost)
+                train_op = optimizer.apply_gradients(grad_and_vars,global_step=global_step)
+                sess.run(tf.global_variables_initializer())
+                def run(train_input,is_training=True):
+                    x_data,y_data,length_data = train_input
+                    fetches = {'step': global_step,
+                               'cost': classifier.cost,
+                               'accuracy': classifier.accuracy,
+                               'learning_rate': learning_rate,
+                               'final_state': classifier.final_state}
+                    feed_dict = {classifier.x: x_data,
+                                 classifier.y: y_data,
+                                 classifier.sequence_length: length_data,
+                                 classifier.batch_size: len(x_data)}
+                    vars = sess.run(fetches, feed_dict)
+                    step = vars['step']
+                    cost = vars['cost']
+                    accuracy = vars['accuracy']
+                    if is_training:
+                        fetches['train_op'] = train_op
+                        feed_dict[classifier.keep_prob] = 0.5
+                    else:
+                        feed_dict[classifier.keep_prob] = 1.0
+                    time_str = datetime.datetime.now().isoformat()
+                    print("{}: step: {}, loss: {:g}, accuracy: {:g}".format(time_str, step, cost, accuracy))
+                    return accuracy
+                for train_input in train_data:
+                    run(train_input,is_training=True)
+                    current_step = tf.train.global_step(sess,global_step)
             
 train() 
